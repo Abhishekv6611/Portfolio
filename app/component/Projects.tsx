281f4e50abf8df827808511ddbacdf7098fd5df3
Abhishekv6611 <abiabhi810@gmail.com>
2025-03-24 20:35:03 +0530

    added description

--- app/component/Projects.tsx
+++ app/component/Projects.tsx
@@ -12,25 +12,25 @@ function Projects() {
     {
       id:1,
       title:'E-Store',
-      description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores rerum debitis adipisci, odio minus ipsum suscipit molestiae ipsam hic ex fuga consequatur sit, illum nostrum enim harum perferendis. Veniam, incidunt.',
+      description:'Developed a fully functional e-commerce store using Open API for product data and implemented Redux for state management, including cart and wishlist functionalities.Ensured a responsive and user-friendly UI with efficient API handling',
       link:'https://react-store-eight-topaz.vercel.app/',
       image:EStore
     },{
       id:2,
       title:'Blogging Site',
-      description:'loream ipsum dolor sit amet consectetur, adipisicing elit. Asperiores rerum debitis adipisci, odio minus ipsum suscipit molestiae ipsam hic ex fuga consequatur sit, illum nostrum enim harum perferendis. Veniam, incidunt.',
+      description:"Developed a secure authentication web app using the MERN stack, featuring user registration login forgot password and reset password functionalities. Integrated Jwt authentication for seamless user management and implemented Gmail SMTP (Nodemailer) for password recovery.",
       link:' https://lazythoughts-client.vercel.app/',
       image:Bloggin
     },{
       id:3,
       title:'Music Player',
-      description:'loream ipsum dolor sit amet consectetur, adipisicing elit. Asperiores rerum debitis adipisci, odio minus ipsum suscipit molestiae ipsam hic ex fuga consequatur sit, illum nostrum enim harum perferendis. Veniam, incidunt.',
+      description:'Designed and developed a dynamic music app using React.js for the frontend and JSON-server as the backend. The app allows users to upload and manage YouTube URLs, song names, and video links efficiently. Integrated Axios for seamless communication between the client and server,ensuring smooth data handling and a user-friendly experience',
       link:' https://music-player-app-jet.vercel.app/',
       image:MusicApp
     },{
       id:4,
       title:'Chat App',
-      description:'loream ipsum dolor sit amet consectetur, adipisicing elit. Asperiores rerum debitis adipisci, odio minus ipsum suscipit molestiae ipsam hic ex fuga consequatur sit, illum nostrum enim harum perferendis. Veniam, incidunt.',
+      description:'Developed a responsive real-time chat application using the MERN stack. The application allows users to securely register, log in, and engage in real-time conversations with other users.Built with React.js and styled using Tailwind CSS for a modern and responsive user interface.Backend Developed using Node.js and Express.js for server-side logic',
       link:' https://chatapp-eomm.onrender.com/',
       image:ChatApp
     }
