0f0fd4c6f1721ea4520da42a559699d9a949f51e
Abhishekv6611 <abiabhi810@gmail.com>
2025-04-01 19:39:11 +0530

    removed unneccesary imp pack

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import {  Jersey_10 } from "next/font/google";
 import "./globals.css";
-import { ReactLenis, useLenis } from 'lenis/react'
+import { ReactLenis } from 'lenis/react'
 const jerseyFont = Jersey_10({
   weight: '400', // Example weight, adjust as needed
   subsets: ['latin'], // Or other relevant subsets
