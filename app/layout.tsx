d765fe711a85424732a0b8c20c9ba585795a9b8c
Abhishekv6611 <abiabhi810@gmail.com>
2025-04-01 19:36:09 +0530

    added cursor smoothnes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import {  Jersey_10 } from "next/font/google";
 import "./globals.css";
-
+import { ReactLenis, useLenis } from 'lenis/react'
 const jerseyFont = Jersey_10({
   weight: '400', // Example weight, adjust as needed
   subsets: ['latin'], // Or other relevant subsets
@@ -18,14 +18,18 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) 
+{
   return (
     <html lang="en" className={`${jerseyFont.variable}`}>
+        <ReactLenis root>
       <body
        className="antialiased"
-      >
+       
+       >
         {children}
       </body>
+        </ReactLenis>
     </html>
   );
 }
