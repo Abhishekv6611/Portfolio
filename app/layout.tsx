f53052512f03a8973134e292a15c1009b93093eb
Abhishekv6611 <abiabhi810@gmail.com>
2025-05-05 16:30:48 +0530

    updated : metadata abhishek to abhishekv

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,11 +12,11 @@ const jerseyFont = Jersey_10({
 
 
 export const metadata: Metadata = {
-  title: "Abhishek | MERN Stack Web Developer & React.js Specialist",
+  title: "Abhishek V | MERN Stack Web Developer & React.js Specialist",
   description:
-    "Explore Abhishek's portfolio featuring projects built with React.js, Node.js, and Tailwind CSS. MERN stack developer available for freelance and full-time opportunities.",
+    "Explore Abhishek V's portfolio featuring projects built with React.js, Node.js, and Tailwind CSS. MERN stack developer available for freelance and full-time opportunities.",
   keywords: [
-    "Abhishek",
+    "Abhishek V",
     "MERN Stack Developer",
     "React.js Developer",
     "Node.js",
@@ -26,19 +26,19 @@ export const metadata: Metadata = {
     "Freelance Web Developer",
     "JavaScript Developer",
   ],
-  authors: [{ name: "Abhishek" }],
+  authors: [{ name: "Abhishek V" }],
   openGraph: {
-    title: "Abhishek | MERN Stack Web Developer",
+    title: "Abhishek V | MERN Stack Web Developer",
     description:
-      "Portfolio of Abhishek, showcasing React.js, Node.js, and MERN stack projects. Contact for freelance or full-time web development roles.",
+      "Portfolio of Abhishek V, showcasing React.js, Node.js, and MERN stack projects. Contact for freelance or full-time web development roles.",
     url: "https://portfolio-taupe-rho-75.vercel.app", // 🔄 Replace with your real URL
-    siteName: "Abhishek Portfolio",
+    siteName: "Abhishek V Portfolio",
     images: [
       {
         url: "https://res.cloudinary.com/dau1kgi0c/image/upload/v1740365580/pgd4g9pedzmln5xmzjmw.png", // 🔄 Replace with your OG image URL
         width: 1200,
         height: 630,
-        alt: "Abhishek Portfolio Preview",
+        alt: "Abhishek V Portfolio Preview",
       },
     ],
     locale: "en_US",
@@ -46,9 +46,9 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Abhishek | MERN Stack Web Developer",
+    title: "Abhishek V | MERN Stack Web Developer",
     description:
-      "Check out Abhishek's React.js and Node.js projects. MERN stack developer open for freelance work and full-time roles.",
+      "Check out Abhishek V's React.js and Node.js projects. MERN stack developer open for freelance work and full-time roles.",
     images: ["https://res.cloudinary.com/dau1kgi0c/image/upload/v1740365580/pgd4g9pedzmln5xmzjmw.png"], // 🔄 Replace with your OG image URL
   },
   metadataBase: new URL("https://portfolio-taupe-rho-75.vercel.app"), // 🔄 Replace with your domain
